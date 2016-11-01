eef89c5f481c7be70bfc3db66da3470aea5ea0f2
Gleidson Brito Santana <gleidsonbrito.ads@gmail.com>
2016-11-01 10:58:03 -0200

    Adição de asserção, validando correção de bug no projeto nfephp-org/sped-common.

--- tests/Auxiliar/IdentifyTest.php
+++ tests/Auxiliar/IdentifyTest.php
@@ -14,5 +14,6 @@ class IdentifyTest extends PHPUnit_Framework_TestCase
         $filePath = dirname(dirname(__FILE__)) . '/fixtures/xml/0008-cte.xml';
         $schem = IdentifyCTe::identificar($filePath, $aResp);
         $this->assertEquals($schem, 'cte');
+        $this->assertEquals('51121006145774000197570010000000131004100705', $aResp['chave']);
     }
 }
