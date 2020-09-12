7d3b8ff69e936f12ebf22322ea53f01d7b262b39
mastercrash32 <49419418+mastercrash32@users.noreply.github.com>
2020-09-12 16:02:45 -0300

    Update Standardize.php
    Permitir carregar o protCTe que nao estava contida na $rootTagList

--- src/Common/Standardize.php
+++ src/Common/Standardize.php
@@ -54,7 +54,8 @@ class Standardize
         'CTe',
         'CTeOS',
         'retConsStatServCte',
-        'cteDistDFeInteresseResponse'
+        'cteDistDFeInteresseResponse',
+        'protCTe'
     ];
 
     /**
