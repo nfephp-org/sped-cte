91e149e0c5bfa4851d163c11ba26ae79f1747559
Guilherme Miranda <guilherme.amiranda@gmail.com>
2025-12-29 13:04:53 -0300

    Ajuste gTribCompraGov dentro de gIBSCBS

--- src/MakeCTeSimp.php
+++ src/MakeCTeSimp.php
@@ -541,6 +541,9 @@ class MakeCTeSimp
                 if (isset($this->gTribRegular)) {
                     $this->dom->appChild($node, $this->gTribRegular, 'Falta tag "gIBSCBS"');
                 }
+                if (isset($this->gTribCompraGov)) {
+                    $this->dom->appChild($node, $this->gTribCompraGov, 'Falta tag "gIBSCBS"');
+                }
                 if (isset($this->gEstornoCred)) {
                     $this->dom->appChild($this->IBSCBS, $this->gEstornoCred, 'Falta tag "IBSCBS"');
                 }
